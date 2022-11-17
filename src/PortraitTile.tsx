cccde356afc193f37bfef23314b18bcbefe4e748
kaveh <kavehcito@users.noreply.github.com>
2022-11-17 01:35:38 -0500

    add tribute icons

--- src/PortraitTile.tsx
+++ src/PortraitTile.tsx
@@ -2,6 +2,9 @@
 import React from 'react';
 export default function PortraitTile({ data }: any) {
   const images = data["Photos/عکس ها"]?.split(',');
+  const tributes = data["Tributes"]?.split((','));
+  console.log("TRIBS", tributes)
+
   return (
     <div className="portrait-tile">
       <div className="column">
@@ -19,7 +22,31 @@ export default function PortraitTile({ data }: any) {
           {data["داستان"]}
         </p>
       </div>
+      <div className="tributes">
+        {tributes?.map(tributeUrl => {
+          let iconClassNames;
+          if (tributeUrl.includes("twitter")) {
+            iconClassNames = "fa-brands fa-twitter";
+          }
+          else if (tributeUrl.includes("instagram")) {
+            iconClassNames = "fa-brands fa-instagram"
+          }
+          else if (tributeUrl.includes("reddit")) {
+            iconClassNames = "fa-brands fa-reddit"
+          }
+          else if (tributeUrl.includes("tiktok")) {
+            iconClassNames = "fa-brands fa-tiktok"
+          }
+          else {
+            iconClassNames = "fa fa-globe"
+          }
+          return (
+            <a target="_blank" style={{"fontSize":24}} href={tributeUrl}>
+              <i className={iconClassNames} />
+            </a>)
+        })}
 
+      </div>
     </div>)
 }
 
